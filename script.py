0651d3bb0d93b3de51fc3eaf2525ccf099eb7e64
Natt Decodes <113068344+nattdecodes@users.noreply.github.com>
2023-05-29 21:29:16 -0400

    Updated Language Processing
    Better structure

--- script.py
+++ script.py
@@ -56,23 +56,50 @@ def check_vocab(possible_msg):
 
 def decrypt_all(encrypted_msg):
     key = 25 # All possible keys
-    possible_msgs = []
-    sorted_decrypted = []
+    v_sorted_dec = []
+    i_sorted_dec = []
     top_chosen_nbr = 5 # Only decrypts the first n messages based on letter distribution
-    while key > 0: # Decrypts every possible keys
+    min_hits = 2 # The minimum amount of hits a decrypted message has to have to be considered the final message
+
+    # Decrypt every possible keys
+    while key > 0:
         decrypted_gibberish = cipher_decrypt(encrypted_msg, key)
-        possible_msgs.append(decrypted_gibberish)
-        possible_msgs.append(key)
+        v_sorted_dec.append(decrypted_gibberish)
+        v_sorted_dec.append(key)
         key -= 1
-    possible_msgs = [possible_msgs[x:x+2] for x in range(0, len(possible_msgs), 2)] # Group each message with its key
-    for item in possible_msgs:
-        item.append(letter_distr(item[0])) # Get the letter distribution index for each message
-    sorted_decrypted = (sorted(possible_msgs, key=lambda x: abs(x[2] - 1)))[:top_chosen_nbr] # Sort messages by index and pick the n first ones
-    for top_poss_msg in sorted_decrypted:
-        check_vocab(top_poss_msg)
-    possible_msgs = sorted(possible_msgs, key=lambda x:x[2], reverse=True) # Sort messages by the vocab check as it's more accurate
-    print(f"deciphered message > {sorted_decrypted[0][0]}")
-    print(f"key > {sorted_decrypted[0][1]}")
+
+    # Group each message with its key
+    v_sorted_dec = [v_sorted_dec[x:x+2] for x in range(0, len(v_sorted_dec), 2)]
+
+    # Get the letter distribution index for each message
+    for item in v_sorted_dec:
+        item.append(letter_distr(item[0]))
+
+    # Sort messages by index and pick the n first ones
+    i_sorted_dec = sorted(v_sorted_dec, key=lambda x: abs(x[2] - 1))
+
+    while True:
+
+        # Check Vocab for the top n keys
+        for top_msg in i_sorted_dec[:top_chosen_nbr]:
+            check_vocab(top_msg)
+
+        # Do nothing for the bottom n
+        for bottom_msg in i_sorted_dec[top_chosen_nbr:]:
+            bottom_msg.append(0)
+
+        # Sort messages by the vocab check
+        v_sorted_dec = sorted(v_sorted_dec, key=lambda x:x[-1], reverse=True)
+
+        # Check if there's no more keys or if the minimum amount of hits is.. well... hit
+        if top_chosen_nbr >= 25 or v_sorted_dec[0][-1] >= min_hits:
+            i_sorted_dec = v_sorted_dec
+            break
+
+        top_chosen_nbr += top_chosen_nbr
+
+    print(f"deciphered message > {v_sorted_dec[0][0]}")
+    print(f"key > {i_sorted_dec[0][1]}")
 
 # Format: ["possible message", keynbr, indexdistrnbr, hitsnbr]
 decrypt_all(input("caesar cipher to decipher > "))
